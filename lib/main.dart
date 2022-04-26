8b19d573e130fe1a2de11c29d671a94443dce963
Rafael Ramos <rafael.ramosrc@gmail.com>
2022-04-26 09:34:29 -0300

    estudo 26/04/2022

--- lib/main.dart
+++ lib/main.dart
@@ -9,51 +9,68 @@ main() {
 class PerguntaAppState extends State<PerguntaApp> {
   var perguntaSelecionada = 0;
 
+  final List<Map> perguntas = const [
+    {
+      'texto': 'Qual sua cor favorita?',
+      'respostas': ['Verde', 'Vermelho', 'Preto', 'Branco'],
+    },
+    {
+      'texto': 'Qual seu animal favorito?',
+      'respostas': ['Cachorro', 'Gato', 'Papagaio', 'Mico'],
+    },
+    {
+      'texto': 'Qual seu carro favorito?',
+      'respostas': ['Gol', 'Uno', 'Chevette', 'Renault Kangoo'],
+    }
+  ];
+
   void responder() {
-    setState(() {
-      perguntaSelecionada++;
-    });
+    if (temPerguntaSelecionada) {
+      setState(() {
+        perguntaSelecionada++;
+      });
+    }
+  }
+
+  bool get temPerguntaSelecionada {
+    return perguntaSelecionada < perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<Map> perguntas = [
-      {
-        'texto': 'Qual sua cor favorita?',
-        'respostas': ['Verde', 'Vermelho', 'Preto', 'Branco'],
-      },
-      {
-        'texto': 'Qual seu animal favorito?',
-        'respostas': ['Cachorro', 'Gato', 'Papagaio', 'Mico'],
-      },
-      {
-        'texto': 'Qual seu carro favorito?',
-        'respostas': ['Gol', 'Uno', 'Chevette', 'Renault Kangoo'],
-      }
-    ];
-    final resposta = [
-      'Resposta 1',
-      'Resposta 2',
-      'Resposta 3',
-      'Resposta 4',
-    ];
+    /* 
+    List<Widget> respostas = [];
+    for (String textoResp in perguntas[perguntaSelecionada]['respostas']) {
+      respostas.add(Resposta(textoResp, responder)); Um exemplo mais imperativo
+    }*/
+
+    List<String>? respostas = temPerguntaSelecionada
+        ? perguntas[perguntaSelecionada]['respostas']
+        : [];
+
+    List<Widget> widgets =
+        respostas!.map((t) => Resposta(t, responder)).toList();
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Perguntas'),
+          title: const Text('Perguntas'),
           centerTitle: true,
           backgroundColor: Colors.purple,
         ),
-        body: Column(
-          children: [
-            Questao(perguntas[perguntaSelecionada]['texto']),
-            Resposta(resposta[0], responder),
-            Resposta(resposta[1], responder),
-            Resposta(resposta[2], responder),
-            Resposta(resposta[3], responder),
-          ],
-        ),
+        body: temPerguntaSelecionada
+            ? Column(
+                children: <Widget>[
+                  Questao(perguntas[perguntaSelecionada]['texto'].toString()),
+                  ...widgets,
+                ],
+              )
+            : const Center(
+                child: Text(
+                  'Parabéns!',
+                  style: TextStyle(fontSize: 28),
+                ),
+              ),
       ),
     );
   }
